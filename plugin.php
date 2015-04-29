93524ab3fc88c16c02da3422cc4bca5503271225
Tim Field <tim@thefold.co.nz>
2015-04-29 22:56:47 +1200

    Plugin version

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: WordPress Dispatcher.
  * Plugin URI: https://github.com/timbofield/wordpress-dispatcher
  * Description: Create URL endpoints within WordPress
- * Version: 1.0.0
+ * Version: 1.0
  * Author: Tim Field
  * Author URI: http://www.thefold.co.nz
  * License: MIT
